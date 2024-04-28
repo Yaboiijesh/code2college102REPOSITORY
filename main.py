0b292064a5050903f57ca17390ee0337e18e196b
Yaboiijesh <swainbrothers@outlook.com>
2024-04-28 13:47:44 -0700

    added path

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-show databases
 import mysql.connector
 
-def menu():
-  print("--------------------------------------------")
-  print("1. View Account Balances ")
-  print("2. Deposit ")
-  print("3. Withdraw ")
-  print("4. Delete account")
-  print("5. Make accoount")
-  print("6. Change account details")
-  print("--------------------------------------------")
-
-connection = mysql.connector.connect(
-        user = 'root', 
-        database = 'sample', 
-        password = 'poop1234'
+connection= connection = mysql.connector.connect(
+    user= 'root',
+    database= 'sample',
+    password= '2gd4fart'
 )
+
+cursor = connection.cursor()
+testQuery = ("SELECT * FROM bank_info")
+cursor.execute(testQuery)
+ 
+
+for row in cursor:
+    print(row)
+
+cursor.close()
+connection.close()
