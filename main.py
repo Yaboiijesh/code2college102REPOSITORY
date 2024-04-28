09d9a87aa0e0c0f339ceec06d075b34fbef5e8bb
Yaboiijesh <swainbrothers@outlook.com>
2024-04-28 13:44:56 -0700

    MYSQL Connect

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello")
+show databases
+import mysql.connector
+
+def menu():
+  print("--------------------------------------------")
+  print("1. View Account Balances ")
+  print("2. Deposit ")
+  print("3. Withdraw ")
+  print("4. Delete account")
+  print("5. Make accoount")
+  print("6. Change account details")
+  print("--------------------------------------------")
+
+connection = mysql.connector.connect(
+        user = 'root', 
+        database = 'sample', 
+        password = 'poop1234'
+)
